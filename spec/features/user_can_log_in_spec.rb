a2cfacefe7924875d19738c79d11bbdf82fc7413
slague <stephanielague@gmail.com>
2017-07-17 11:51:17 -0600

    Complete logout feature
    *remove confirmation field from login (only required for create account)

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'A registered user that is NOT logged in visits root' do
   before do
     @user = User.create(name: "Person", password: "123", email: "hi@gmail.com")
-    @my_link = @user.links.create(title: "First Link", url: "https://google.com", read: true)
   end
 
   scenario "is redirected to home page to log in" do
@@ -18,21 +17,18 @@ describe 'A registered user that is NOT logged in visits root' do
     visit login_path
     fill_in "Email", with: @user.email
     fill_in "Password", with: @user.password
-    fill_in "Password confirmation", with: @user.password
 
     click_on "Login"
 
     expect(current_path).to eq(links_path)
     expect(page).to have_content("Signed with #{@user.email} account")
     expect(page).to have_content("Logout")
-    # expect(page).to have_content(@my_link)
   end
 
   scenario "cannot log in with incorrect email-passoword combination" do
     visit login_path
     fill_in "Email", with: @user.email
     fill_in "Password", with: '456'
-    fill_in "Password confirmation", with: '456'
 
     click_on "Login"
 
@@ -43,24 +39,9 @@ describe 'A registered user that is NOT logged in visits root' do
     visit login_path
     fill_in "Email", with: 'anewemail@gmail.com'
     fill_in "Password", with: '456'
-    fill_in "Password confirmation", with: '456'
 
     click_on "Login"
 
     expect(page).to have_content("No account registered with that email. Please sign up.")
   end
-
 end
-
-
-
-
-
-#
-# Sign In
-#
-# As a registered user, when I attempt to sign in, I receive a flash message to reflect a successful or unsuccessful log in attempt.
-#
-# Sign Out
-#
-# As an authenticated user viewing the index page, I should see a link to "Sign Out" and not see a link to "Sign In". This "Sign Out" link should redirect me back to the page that prompts me to "Log In or Sign Up".
